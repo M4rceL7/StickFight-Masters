76309080fbc5ebb98b6312b8309572bd84d4f1a9
Marcel Triebe <M4rcel2012@gmail.com>
2024-10-01 16:51:10 +0200

    Update project

--- App/Source/Main.cpp
+++ App/Source/Main.cpp
@@ -1,6 +1,23 @@
-#include "Core/Core.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Config.hpp>
+#include <SFML/GpuPreference.hpp>
+#include <sfml/Main.hpp>
+#include <sfml/Window.hpp>
+#include <sfml/OpenGL.hpp>
+#include <sfml/System.hpp>
+#include <sfml/System.hpp>
+#include <iostream>
+#include <cmath>
+#include <string>
+
+
+
+
 
 int main()
 {
-	Core::PrintHelloWorld();
+    
+    
+
 }
